2c70f7f72286fbd6ff25312f023bec0c3ee5d2fc
Ericphoto35 <ericdu974@gmail.com>
2025-03-16 19:55:18 +0100

    Exercise Js

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,12 @@
 //Votre code ici
-
+/**
+ * Additionne deux nombres
+ * @param {number|string} a - Premier nombre (entier ou décimal)
+ * @param {number|string} b - Deuxième nombre (entier ou décimal)
+ * @returns {number} La somme des deux nombres
+ */
+function add(a, b) {
+    return Number(a) + Number(b);
+  }
+  
  export default add;
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,16 @@
 // votre code ici
-
+/**
+ * Vérifie l'âge d'une personne et renvoie un message en fonction de la tranche d'âge.
+ * @param {number} age - L'âge de la personne.
+ * @returns {string} - Un message indiquant la tranche d'âge.
+ */
+function checkAge(age) {
+    if (age < 18) {
+      return "Vous êtes mineur.";
+    } else if (age >= 18 && age < 65) {
+      return "Vous êtes majeur.";
+    } else {
+      return "Vous êtes senior.";
+    }
+  }
 export default checkAge;
